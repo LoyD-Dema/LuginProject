60091514cfda226ef877256e8d7c4de6dc775db5
P0stMelone <riccardo.melone@outlook.com>
2026-05-30 14:00:37 +0200

    Implemented Selection with Mouse

--- Assets/_Scripts/UI/MenuNavigation.cs
+++ Assets/_Scripts/UI/MenuNavigation.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
@@ -14,34 +16,36 @@ public class MenuNavigation : MonoBehaviour
     [SerializeField] float clickDelay = 0.2f;
 
     private int currentIndex = 0;
-    private bool bIsClicking;
+    private bool bIsClicking = false;
 
+    //Setto Index a 0 e aggiorno la canvas
     private void OnEnable()
     {
-        if (buttons.Length == 0 ||!arrowVisual) return;
+        if (buttons.Length == 0 || !arrowVisual) return;
 
         currentIndex = 0;
-        Canvas.ForceUpdateCanvases();
         UpdateMenuVisual();
     }
-
+    #region Navigation
+    //Ricevo i comandi dall'InputSystem con W vado verso l'alto con S verso il basso nel menu
     public void OnNavigate(InputValue value)
     {
         if (buttons.Length == 0) return;
-        
+
         Vector2 direction = value.Get<Vector2>();
         if (direction.y < -0.1f)
         {
             currentIndex = (currentIndex + 1) % buttons.Length;
             UpdateMenuVisual();
         }
-        else if(direction.y > 0.1f)
+        else if (direction.y > 0.1f)
         {
             currentIndex = (currentIndex - 1 + buttons.Length) % buttons.Length;
             UpdateMenuVisual();
         }
     }
 
+    //Tramite InputSystem se il valore ì true chiamo La conferma di selezione
     public void OnSubmit(InputValue value)
     {
         if (value.isPressed)
@@ -50,6 +54,37 @@ public class MenuNavigation : MonoBehaviour
         }
     }
 
+    //chiamo la Coroutine per l'animazione del bottone
+    private void ConfirmSelection()
+    {
+        if (buttons == null) return;
+
+        StartCoroutine(ClickCoroutine());
+    }
+
+    //viene chiamata dai bottoni quando ci si passa sopra
+    //se il bottone selezionato è differente da quello precedente setto l'indice
+    public void HandlePointerEnter(Button button)
+    {
+        if (bIsClicking) return;
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (buttons[i] == button)
+            {
+                if (currentIndex != i)
+                {
+                    currentIndex = i;
+                    UpdateMenuVisual();
+                }
+            }
+        }
+    }
+#endregion
+    #region Visual Update
+
+    //Setto la posizione delle frecce laterali sul bottone
+    //Il ciclo è per forzare lo stato dei bottoni e applicare una leggera sfumatura
     private void UpdateMenuVisual()
     {
         RectTransform currentButton = buttons[currentIndex].GetComponent<RectTransform>();
@@ -68,14 +103,11 @@ public class MenuNavigation : MonoBehaviour
             }
         }
     }
+    #endregion
 
-    private void ConfirmSelection()
-    {
-        if (buttons == null) return;
-
-        StartCoroutine(ClickCoroutine());
-    }
-
+    #region Coroutine
+    //coroutine per cambiare lo sprite del bottone e delle frecce
+    //finita la coroutine viene invocato il funzionamento dei bottoni
     private IEnumerator ClickCoroutine()
     {
         bIsClicking = true;
@@ -96,4 +128,5 @@ public class MenuNavigation : MonoBehaviour
         currentButton.onClick.Invoke();
 
     }
+    #endregion
 }
