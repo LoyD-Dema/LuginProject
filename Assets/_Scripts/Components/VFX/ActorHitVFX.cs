4c0243c0d012c467e75aecedc213fd5be13eab9b
Martina Tenani <44113972+martinatenani@users.noreply.github.com>
2026-06-02 11:25:39 +0200

    made shader for damage

---
+++ Assets/_Scripts/Components/VFX/ActorHitVFX.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.VFX;
+using Utilities;
+
+namespace Components.VFX
+{
+    /// <summary>
+    /// This component is responsible for handling the visual effects that occur when an Actor receives a hit.
+    /// It listens for the HitReceived event from the Actor and triggers the appropriate visual effects,
+    /// such as playing hit particles and applying a hit shader to the Actor's material.
+    /// The component also ensures that the visual effects are properly cleaned up after they have finished playing.
+    /// </summary>
+    [RequireComponent(typeof(Actor))]
+    public class ActorHitVFX : MonoBehaviour
+    {
+        private Actor owningActor;
+        [SerializeField] private VisualEffectAsset hitParticlesAsset; //particles to play when hit
+        [SerializeField] private Shader hitShader; //a shader to apply to the Actor when hit
+        
+        #region Utilities
+        private GameObject go;
+        private VisualEffect HitParticlesVfx;
+        private float hitEffcetDuration = 1f; //duration of the hit effect, we can adjust this based on the actual duration of the particle system or shader effect
+        private float lifetimeBuffer = .4f; //a small buffer to ensure that the effect is completely finished before destroying the game object
+        #endregion 
+        
+        private void OnEnable()
+        {
+            owningActor = GetComponent<Actor>();
+            owningActor.HitReceived += OnHit;
+        }
+
+        private void OnDisable()
+        {
+            owningActor.HitReceived -= OnHit;
+        }
+
+        private void OnHit(HitInfo hitInfo)
+        {
+            go = new GameObject("HitEffectsVFX");
+            go.transform.position = hitInfo.HitPoint;
+            
+            Debug.Log("VFX position: " + go.transform.position);
+            
+            HitParticlesVfx = go.AddComponent<VisualEffect>();
+            HitParticlesVfx.visualEffectAsset = hitParticlesAsset;
+            
+            HitParticlesVfx.Play();
+            StartCoroutine(DestroyWhenFinished(go, HitParticlesVfx));
+
+            //flash Material
+            StartCoroutine(RunShaderEffect());
+            //play sound
+        }
+
+        IEnumerator RunShaderEffect()
+        {
+            Renderer renderer = owningActor.GetComponent<Renderer>();
+            Material originalMaterial = renderer.material;
+            renderer.material = new Material(hitShader);
+            yield return new WaitForSeconds(hitEffcetDuration);
+            owningActor.GetComponent<Renderer>().material = originalMaterial;
+        }
+
+        private IEnumerator DestroyWhenFinished(GameObject go, VisualEffect vfx)
+        {
+            // Wait until the effect actually starts emitting
+            yield return null;
+
+            while (vfx.aliveParticleCount == 0)
+                yield return null;
+            
+            while (vfx.aliveParticleCount > 0)
+                yield return null;
+
+            yield return new WaitForSeconds(lifetimeBuffer); //added a small buffer to ensure that the effect is completely finished before destroying the game object
+            
+            Destroy(go);
+        }
+        
+    }
+}
