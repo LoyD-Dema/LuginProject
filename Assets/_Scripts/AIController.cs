1c49264348a2b3bd81c33ff27ce30d46f6e64331
Simone Orlini <simone.orl04@gmail.com>
2026-06-03 23:51:11 +0200

    Added shooting logic and crude FSM.

--- Assets/_Scripts/AIController.cs
+++ Assets/_Scripts/AIController.cs
@@ -1,30 +1,77 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AIController : MonoBehaviour
 {
+    protected enum States
+    {
+        None,
+        Moving,
+        Shooting,
+        LAST
+    }
+
     [SerializeField]
     private Transform target;
     [SerializeField]
-    private float shootRadiusSquared;
+    private float shootRadius = 5;
+    [SerializeField]
+    private float followRadius = 10; // When the enemy starts shooting it will go back to the follow state when this distance is crossed
+
+    // DEBUG
+    private Dictionary<States, Color> statesColors = new Dictionary<States, Color>((int)States.LAST);
+
+    private States currentState = States.Moving;
 
     private MovementComponent movementComponent;
+    private ShootComponent shootComponent;
+    private Material material;
 
     private void Start()
     {
         movementComponent = GetComponent<MovementComponent>();
+        shootComponent = GetComponent<ShootComponent>();
+        material = GetComponent<Renderer>().material;
+
+        statesColors[States.Moving] = Color.blue;
+        statesColors[States.Shooting] = Color.red;
     }
 
     private void Update()
     {
+        if (!target) return;
+
         Vector3 dst = target.position - transform.position;
         transform.forward = dst.normalized;
 
-        if(dst.sqrMagnitude >= shootRadiusSquared * shootRadiusSquared)
+        switch (currentState)
         {
-            movementComponent.SetDirection(new Vector2(transform.forward.x, transform.forward.z));
-            return;
+            case States.Moving:
+                if (dst.sqrMagnitude <= shootRadius * shootRadius)
+                {
+                    currentState = States.Shooting;
+                    movementComponent.SetDirection(Vector2.zero);
+                    break;
+                }
+
+                movementComponent.SetDirection(new Vector2(transform.forward.x, transform.forward.z));
+
+                break;
+            case States.Shooting:
+                
+                shootComponent.Shoot();
+                
+                if (dst.sqrMagnitude > followRadius * followRadius)
+                {
+                    currentState = States.Moving;
+                }
+
+                break;
+            default:
+                Debug.Log("Nessuno stato trovato :c");
+                break; 
         }
 
-        movementComponent.SetDirection(Vector2.zero);
+        material.color = statesColors[currentState];
     }
 }
