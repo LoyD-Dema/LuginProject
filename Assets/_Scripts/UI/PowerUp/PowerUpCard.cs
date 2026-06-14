3c0a0681032e04bf42c90b03450aca0abb0a0312
P0stMelone <riccardo.melone@outlook.com>
2026-06-14 13:37:38 +0200

    Implementato gestione dei potenziamenti
    Bug durante l'apertura del menu, si apre una volta ogni 2 livelli

--- Assets/_Scripts/UI/PowerUp/PowerUpCard.cs
+++ Assets/_Scripts/UI/PowerUp/PowerUpCard.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PowerUpCard : MonoBehaviour
 {
+    [SerializeField] private PowerUpManager powerUpManager;
+
     [SerializeField] private TMP_Text nameText;
     [SerializeField] private TMP_Text descriptionText;
     [SerializeField] private Image icon;
 
+    private PowerUpData data;
+    private float value;
+
     public void SetupCard(PowerUpData data)
     {
+        this.data = data;
+        value = data.RollModifier(data.Modifier);
+
         nameText.text = data.PowerUpName;
         icon.sprite = data.Icon;
 
-        descriptionText.text = data.GetFormattedDescription(data.RollModifier(data.Modifier));
+        descriptionText.text = data.GetFormattedDescription(value);
+    }
+
+    public void OnCardSelected()
+    {
+        powerUpManager.ApplyPowerUp(data, value);
     }
 }
