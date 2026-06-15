c3affe5b7deb35402981bfdda101f9332fc844ae
LoyD <lorenzo180904@gmail.com>
2026-06-15 11:18:46 +0200

    Add BullePool

---
+++ Assets/_Scripts/Managers/BulletPool.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class BulletPool : MonoBehaviour
+{
+    [SerializeField] private BulletBehavior bulletPrefab;
+    [SerializeField][Range(1, 100)] private int capacity = 12;
+    [SerializeField][Range(1, 100)] private int maxSize = 24;
+
+    private ObjectPool<BulletBehavior> pool;
+
+    private void Awake()
+    {
+        pool = new ObjectPool<BulletBehavior>(
+            createFunc: CreateItem,
+            actionOnGet: OnGetItem,
+            actionOnRelease: OnReleaseItem,
+            actionOnDestroy: OnDestroyItem,
+            collectionCheck: true,
+            defaultCapacity: capacity,
+            maxSize: maxSize
+        );
+    }
+
+    private void Start()
+    {
+        CreateItem();
+    }
+
+    private BulletBehavior CreateItem()
+    {
+        BulletBehavior bullet = Instantiate(bulletPrefab);
+        bullet.name = "Bullet";
+        bullet.gameObject.SetActive(false);
+        return bullet;
+    }
+
+    //Get a bullet from the pool
+    public void OnGetItem(BulletBehavior bullet)
+    {
+        bullet.gameObject.SetActive(true);
+    }
+
+    private void OnReleaseItem(BulletBehavior bullet)
+    {
+        bullet.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyItem(BulletBehavior bullet)
+    {
+        Destroy(bullet.gameObject);
+    }
+}
