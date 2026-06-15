f829365586f12a82cee3cc7295cdbde36d53aaeb
Martina Tenani <44113972+martinatenani@users.noreply.github.com>
2026-06-15 10:18:44 +0200

    refactoring code

---
+++ Assets/_Scripts/Components/CombatEvents.cs
@@ -0,0 +1,9 @@
+using System;
+
+/// <summary>
+/// A collection of GLOBAL EVENTS to regulate combat.
+/// </summary>
+public static class CombatEvents
+{
+    public static Action<int> OnEnemyKilled;
+}
